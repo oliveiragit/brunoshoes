9236d0c3dd9378e8ae914989dca451e6d7ee27ef
oliveiragit <deoliveirax@hotmail.com>
2020-08-11 11:52:37 -0300

    add hide files

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+  },
+  extends: ['plugin:react/recommended', 'airbnb', 'prettier', 'prettier/react'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', 'react-hooks'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'import/prefer-default-export': 'off',
+
+    'no-console': ['error', { allow: ['tron'] }],
+    'react/state-in-constructor': 'off',
+    'no-param-reassign': 'off',
+
+    'react-hooks/rules-of-hooks': 'error', //hooks
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+};
